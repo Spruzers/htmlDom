c2424d2c374c9024f57d8ded2ab252984da8c834
sauban <saubanlawal@gmail.com>
2016-11-12 07:55:23 +0100

    writing into each cards

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 
 var card_container = document.getElementById('card-container'),
 	cards = document.getElementsByClassName('card'),
-	query_cards = document.querySelectorAll('.card'),
+	query_divs = document.querySelectorAll('div'),
 	query_container = document.querySelector('#card-container'),
 	body = document.getElementsByTagName('body');
 console.log(card_container);
 console.log(cards);
-console.log(query_cards);
+console.log(query_divs);
 console.log(query_container);
 console.log(body);
+
+
+for (var i = 0; i < cards.length; i++) {
+	cards[i].innerHTML = '<h1> I am card <b>NUMBER ' + (i + 1) + '</b></h1>';
+
+};
