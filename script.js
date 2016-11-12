dda09117f601917beed3750a4b910164a2b4c3ca
sauban <saubanlawal@gmail.com>
2016-11-12 07:54:09 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var card_container = document.getElementById('card-container'),
+	cards = document.getElementsByClassName('card'),
+	query_cards = document.querySelectorAll('.card'),
+	query_container = document.querySelector('#card-container'),
+	body = document.getElementsByTagName('body');
+console.log(card_container);
+console.log(cards);
+console.log(query_cards);
+console.log(query_container);
+console.log(body);
