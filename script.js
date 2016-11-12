f4c560e8fe5d7fb44d4e903275affb603c394ca5
sauban <saubanlawal@gmail.com>
2016-11-12 08:08:17 +0100

    appending new card element every 1 second

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var card_container = document.getElementById('card-container'),
 	body = document.getElementsByTagName('body');
 
 
-
 setInterval(function  () {
 	card_container.style['background-color'] = 'rgb('
 					+ randColor()
@@ -16,26 +15,23 @@ setInterval(function  () {
 					+ ','
 					+ randColor()
 					+ ')';
+		var new_card = document.createElement('div');
+			new_card.className = 'card';
+		card_container.appendChild(new_card);
+
+		function randColor () {
+			return Math.floor(Math.random() * 255);
+		};
+
+
+		for (var i = 0; i < cards.length; i++) {
+			cards[i].innerHTML = '<h1> I am card <b>NUMBER ' + (i + 1) + '</b></h1>';
+			cards[i].style['background-color'] = 'rgb('
+							+ randColor()
+							+ ','
+							+ randColor()
+							+ ','
+							+ randColor()
+							+ ')';
+		};
 }, 1000);
-
-
-for (var i = 0; i < 10; i++) {
-	var new_card = document.createElement('div');
-		new_card.className = 'card';
-	card_container.appendChild(new_card);
-};
-
-function randColor () {
-	return Math.floor(Math.random() * 255);
-};
-
-for (var i = 0; i < cards.length; i++) {
-	cards[i].innerHTML = '<h1> I am card <b>NUMBER ' + (i + 1) + '</b></h1>';
-	cards[i].style['background-color'] = 'rgb('
-					+ randColor()
-					+ ','
-					+ randColor()
-					+ ','
-					+ randColor()
-					+ ')';
-};
